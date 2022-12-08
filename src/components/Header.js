ae84f1162e72965a98d660b8ea0558a81c39e6eb
root <root@DESKTOP-JFEPESN.localdomain>
2022-12-09 01:00:50 +0200

    Done

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header({isDarkMode, onDarkModeClick}){
+    return(
+        <>
+        <h2>Shopster</h2>
+        <button onClick={onDarkModeClick}>
+          {isDarkMode ? "Dark" : "Light"} Mode
+        </button>
+        </>
+    )
+}
+export default Header
